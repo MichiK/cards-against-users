18dc90f45945a63a9d774b6fb87ce106342c7c11
MichiK <6341204+MichiK@users.noreply.github.com>
2020-07-30 04:47:59 +0200

    Improve code and add some more phrases

--- cards.py
+++ cards.py
@@ -2,11 +2,12 @@
 
 import os
 
-from cgi import escape
+from html import escape
 from random import choice
 from re import search, sub
 
-class Cards():
+
+class Cards:
     """
     The main class that contains all the logic. The files with the contents
     of white and black cards are loaded once, the rest is handled by class
@@ -17,22 +18,22 @@ class Cards():
     blank lines are ignored.
     """
 
-    s = "_REPLACED_S_"
-    e = "_REPLACED_E_"
+    left = "_REPLACED_LEFT_"
+    right = "_REPLACED_RIGHT_"
 
-    d = os.path.dirname(os.path.realpath(__file__))
+    directory = os.path.dirname(os.path.realpath(__file__))
 
     white = []
-    with open(d + "/white.txt", "r", encoding="utf-8") as f:
-        for l in f:
-            if not l.startswith("#") and len(l) > 1:
-                white.append(l.rstrip())
+    with open(directory + "/white.txt", "r", encoding="utf-8") as file:
+        for line in file:
+            if not line.startswith("#") and len(line) > 1:
+                white.append(line.rstrip())
 
     black = []
-    with open(d + "/black.txt", "r", encoding="utf-8") as f:
-        for l in f:
-            if not l.startswith("#") and len(l) > 1:
-                black.append(l.rstrip())
+    with open(directory + "/black.txt", "r", encoding="utf-8") as file:
+        for line in file:
+            if not line.startswith("#") and len(line) > 1:
+                black.append(line.rstrip())
 
     @classmethod
     def draw_white(cls):
@@ -40,7 +41,7 @@ class Cards():
         Draw a white card and return the text written on it.
         """
         return choice(cls.white)
-    
+
     @classmethod
     def draw_black(cls):
         """
@@ -49,70 +50,76 @@ class Cards():
         return choice(cls.black)
 
     @classmethod
-    def remove_article(cls, str):
+    def remove_article(cls, phrase):
         """
-        Remove the article from a string given and return the result.
+        Remove the article from a given phrase and return the result.
         """
         regex = "^(a|an|the) "
-        return sub(regex, "", str)
+        return sub(regex, "", phrase)
 
     @classmethod
-    def to_lowercase(cls, str):
+    def to_lowercase(cls, phrase):
         """
-        Convert a string to lowercase, remove special characters etc.
+        Convert a phrase to lowercase, remove special characters etc.
         and return the result."
         """
-        regex = "[ \/\.]"
-        return sub(regex, "-", cls.remove_article(str).lstrip().lower())
+        regex = "[ /.]"
+        return sub(regex, "-", cls.remove_article(phrase).lstrip().lower())
 
     @classmethod
     def replace(cls, black, white):
         """
         Replace content on a black card with some white card.
-        
+
         This function first looks for special replacements as `__NOARTICLE__`
         or `__LOWERCASE__` and replaces the first one it finds. In the end,
         it tries a standard replacement or returns the text unchanged, if no
         replacement string could be found.
         """
-        s = cls.s
-        e = cls.e
+        left, right = cls.left, cls.right
+
         if "__NOARTICLE__" in black:
             return black.replace(
-                "__NOARTICLE__", s + cls.remove_article(white) + e, 1
+                "__NOARTICLE__", left + cls.remove_article(white) + right, 1
             )
-        elif "__LOWERCASE__" in black:
+
+        if "__LOWERCASE__" in black:
             return black.replace(
-                "__LOWERCASE__", s + cls.to_lowercase(white) + e, 1
+                "__LOWERCASE__", left + cls.to_lowercase(white) + right, 1
             )
-        else:
-            return sub("__[A-Z]*__", s + white + e, black, 1)
-    
+
+        return sub("__[A-Z]*__", left + white + right, black, 1)
+
     @classmethod
-    def get_phrase(cls, html=False):
+    def get_phrase(cls, html: bool = False, left="", right=""):
         """
         Return a catchy phrase.
 
         This function draws a black card and then draws white cards until
-        all replacement pattterns have been filled with content. It then
+        all replacement patterns have been filled with content. It then
         formats the return string a bit so it looks nicer.
         """
-        str = cls.draw_black()
-        while search("__[A-Z_]*__", str):
-            str = cls.replace(str, cls.draw_white())
+        phrase = cls.draw_black()
+        while search("__[A-Z_]*__", phrase):
+            phrase = cls.replace(phrase, cls.draw_white())
 
-        l = len(cls.s)
-        if str.startswith(cls.s) and str[l] != " ":
-            str = str[0:l] + str[l].upper() + str[l+1:]
+        # Capitalize the first letter of the phrase. If the phrase starts
+        # with a white card, we operate at the offset after the replacement
+        # indicator.
+        pos = len(cls.left)
+        if phrase.startswith(cls.left) and phrase[pos] != " ":
+            phrase = phrase[0:pos] + phrase[pos].upper() + phrase[pos+1:]
         else:
-            str = str[0].upper() + str[1:]
+            phrase = phrase[0].upper() + phrase[1:]
 
-        str = str.replace(cls.s + " ", cls.s).lstrip()
+        # If a card starts with a space to prevent capitalization,
+        # we strip that space at this point to not mess up the layout.
+        phrase = phrase.replace(cls.left + " ", cls.left).lstrip()
 
         if html:
-            return escape(str)
-        else:
-            return str.replace(cls.s, "").replace(cls.e, "")
+            phrase = escape(phrase)
+
+        return phrase.replace(cls.left, left).replace(cls.right, right)
 
 
 def init_flask():
@@ -120,16 +127,15 @@ def init_flask():
     Initialize the Flask object and create a route to the template.
     """
     from flask import Flask, render_template
+
     app = Flask(__name__)
+
     @app.route("/")
     def render():
-        return render_template(
-            "cards.html",
-            phrase=Cards.get_phrase(html=True),
-            s=Cards.s, e=Cards.e,
-            markup=True
-        )
+        return render_template("cards.html", cards=Cards, markup=True)
+
     return app
+
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
@@ -140,8 +146,7 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
     if args.debug:
-        app = init_flask()
-        app.run(debug=True)
+        init_flask().run(debug=True)
     else:
         while True:
             try:
